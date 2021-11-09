0870a10625a959174a28090de86c7a81b4200b94
Arsanias <murat.karaguezel@hotmail.com>
2021-11-09 20:23:15 +0100

    Add files via upload

---
+++ JTViewer.dpr
@@ -0,0 +1,18 @@
+// Copyright (c) 2021 Arsanias
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+
+program JTViewer;
+
+uses
+  Vcl.Forms,
+  TestUnit in 'TestUnit.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
